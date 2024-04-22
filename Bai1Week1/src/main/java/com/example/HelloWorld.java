eee43ace1d043b2662eb86d4cecbcbe64ee79adf
Giang1311 <96896545+Giang1311@users.noreply.github.com>
2024-04-22 21:43:12 +0700

    Delete Bai1Week1 directory

--- Bai1Week1/src/main/java/com/example/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Bai1Week1.src.main.java.com.example;
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-    public String getMessage() {
-        return "Hello, World!";
-    }
-}
