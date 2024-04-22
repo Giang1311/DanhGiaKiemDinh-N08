6fc2eb0c66b73d22f70033a07291cce7a41c39e6
Giang1311 <96896545+Giang1311@users.noreply.github.com>
2024-04-22 22:01:26 +0700

    Add files via upload

---
+++ Week1/Bai1Week1/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testGetMessage() {
+        HelloWorld helloWorld = new HelloWorld();
+        
+        String message = helloWorld.getMessage();
+        
+        assertEquals("Hello, World!", message);
+    }
+}
